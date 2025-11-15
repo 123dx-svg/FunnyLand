098dc4e333daf0614d0a5ece865a5d19cb21a848
dingxiao <11756@DESKTOP-2USBMQO>
2025-11-15 15:57:10 +0800

    基础架构和创建基础窗口

---
+++ FunnyLand/src/engine/core/game_app.cpp
@@ -0,0 +1,97 @@
+﻿#include "game_app.h"
+
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_render.h>
+#include <spdlog/spdlog.h>
+
+namespace  engine::core
+{
+    
+GameApp::GameApp() = default;
+
+
+GameApp::~GameApp()
+{
+    if (is_running_)
+    {
+        spdlog::warn("GameApp销毁时,游戏仍在运行,可能会导致资源泄漏...");
+        close();
+    }
+}
+void GameApp::run()
+{
+    if (!init()) {
+        spdlog::error("初始化失败,游戏无法运行");
+        return;
+    }
+    while (is_running_)
+    {
+        float delta_time = 0.0f;
+        handleEvents();
+        update(delta_time);
+        render();
+    }
+    
+    close();
+}
+    
+bool GameApp::init()
+{
+    spdlog::trace("GameApp初始化...");
+    // SDL初始化
+    if (!SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO)) {
+        spdlog::error("SDL 初始化失败: {}", SDL_GetError());
+        return false;
+    }
+    //创建窗口
+    window_ = SDL_CreateWindow("FunnyLand", 1280, 720, SDL_WINDOW_RESIZABLE);
+    if (!window_) {
+        spdlog::error("SDL 创建窗口失败: {}", SDL_GetError());
+        return false;
+    }
+    //创建渲染器
+    renderer_ = SDL_CreateRenderer(window_, nullptr);
+    if (!renderer_) {
+        spdlog::error("SDL 创建渲染器失败: {}", SDL_GetError());
+        return false;
+    }
+    is_running_ = true;
+    return true;
+}
+
+void GameApp::handleEvents()
+{
+    SDL_Event event;
+    if (SDL_PollEvent(&event)) {
+        if (event.type == SDL_EVENT_QUIT) {
+            is_running_ = false;
+        }
+    }
+}
+
+void GameApp::update(float delta_time)
+{
+    
+}
+
+void GameApp::render()
+{
+    
+}
+
+void GameApp::close()
+{
+    spdlog::trace("GameApp关闭...");
+    if (renderer_) {
+        SDL_DestroyRenderer(renderer_);
+        renderer_ = nullptr;
+    }
+    if (window_) {
+        SDL_DestroyWindow(window_);
+        window_ = nullptr;
+    }
+    SDL_Quit();
+    is_running_ = false;
+}
+    
+}
---
+++ FunnyLand/src/engine/core/game_app.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+struct SDL_Window;
+struct SDL_Renderer;
+
+namespace  engine::core
+{
+    class GameApp final // final 表示不能被继承
+    {
+    private:    
+        //SDL 窗口和渲染器
+        SDL_Window* window_ = nullptr;
+        SDL_Renderer* renderer_ = nullptr;
+        bool is_running_ = false;
+        
+    public:
+        GameApp();
+        ~GameApp();
+        
+        //游戏运行程序中间会调用init(),进入主循环，离开循环后调用close()
+        void run();
+        
+        //禁用掉拷贝和移动构造函数
+        GameApp(const GameApp&) = delete;
+        GameApp(GameApp&&) = delete;
+        GameApp& operator=(const GameApp&) = delete;
+        GameApp& operator=(GameApp&&) = delete;
+        
+    private:
+        [[nodiscard]] bool init(); //返回值必须被使用
+        void handleEvents();
+        void update(float delta_time);
+        void render();
+        void close();
+        
+    };
+}
+
